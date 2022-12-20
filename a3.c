d69f10a153e54c2a15da394fb49fe85bf4c6b7a3
Ayushhad <108228706+Ayushhad@users.noreply.github.com>
2022-12-21 01:23:37 +0530

    Update a3.c

--- a3.c
+++ a3.c
@@ -17,17 +17,15 @@ static int __init a3_init(void)
 {
     struct task_struct *t;
     t = pid_task(find_vpid(pid), PIDTYPE_PID);
-    switch(t){
-        case NULL:
-            return -ESRCH;
-        default :
-            printk(KERN_INFO "pid : %d\n", t->pid); int x;
-            printk(KERN_INFO "uid : %d\n", t->cred->uid.val); int y;
-            printk(KERN_INFO "pgid : %d\n", t->group_leader->pid);
-            int z;
-            printk(KERN_INFO "comm : %s\n", t->comm);
-            return 0;
-    }
+    if (t==NUll)    return -ESRCH;
+    printk(KERN_INFO "pid : %d\n", t->pid);
+    int x;
+    printk(KERN_INFO "uid : %d\n", t->cred->uid.val);
+    int y;
+    printk(KERN_INFO "pgid : %d\n", t->group_leader->pid);
+    int z;
+    printk(KERN_INFO "comm : %s\n", t->comm);
+    return 0;
 }
 static void __exit a3_exit(void)
 {
