7a4a29ae05fad794eaeab695acd3253b2a001c1e
Ayushhad <108228706+Ayushhad@users.noreply.github.com>
2022-12-21 02:11:07 +0530

    Update a3.c

--- a3.c
+++ a3.c
@@ -2,11 +2,11 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/unistd.h>
-#include <linux/syscalls.h>
+#include <linux/syscall.h>
 #include <linux/sched.h>
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Ayushhad");
-MODULE_DESCRIPTION("OS_Assignmetn3_SystemCalls");
+MODULE_AUTHOR("SRIDHAR SODHI");
+MODULE_DESCRIPTION("OS ASS3 Q3");
 MODULE_VERSION("0.01");
 
 int pid = 0;
@@ -14,18 +14,21 @@ module_param(pid, int, 0);
 
 static int __init a3_init(void)
 {
-    struct task_struct *t;
-    t = pid_task(find_vpid(pid), PIDTYPE_PID);
-    if (t==NULL)    return -ESRCH;
-    printk(KERN_INFO "pid : %d\n", t->pid);
-    printk(KERN_INFO "uid : %d\n", t->cred->uid.val);
-    printk(KERN_INFO "pgid : %d\n", t->group_leader->pid);
-    printk(KERN_INFO "comm : %s\n", t->comm);
+    struct task_struct *t1;
+    t1 = pid_task(find_vpid(pid), PIDTYPE_PID);
+    if (!t1)
+    {
+        return -ESRCH;
+    }
+    printk(KERN_INFO "The pid is: %d\n", t1->pid);
+    printk(KERN_INFO "The uid is: %d\n", t1->cred->uid.val);
+    printk(KERN_INFO "The pgid is: %d\n", t1->group_leader->pid);
+    printk(KERN_INFO "The comm is: %s\n", t1->comm);
     return 0;
 }
 static void __exit a3_exit(void)
 {
-    printk(KERN_INFO "THANK YOU!\n");
+    printk(KERN_INFO "Bye\n");
 }
 module_init(a3_init);
 module_exit(a3_exit);
